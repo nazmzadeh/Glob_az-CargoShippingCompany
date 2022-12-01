d49d6de9711fb2d82ad2dc4ca3e98090182a3b09
Elnaraa9 <114652840+RafaelNagiyev@users.noreply.github.com>
2022-12-01 14:08:00 +0400

    fixed header

--- src/pages/Footer/Footer.tsx
+++ src/pages/Footer/Footer.tsx
@@ -1,7 +1,31 @@
-import './Footer.scss'
+import "./Footer.scss";
+import logo from "./logo.png";
+import phone from "./phone.svg";
 
-export const Footer=()=>{
-    return(
-      <p>footer</p>
-    );
-  }
+export const Footer = () => {
+  return (
+    <div className="firstDiv">
+      <ul>
+        <li>
+          <a href="#/">
+            <img src={logo} alt="logo" />
+            <li>
+              <div className="header-phone">
+                <img src={phone} alt="" />
+                <a href="tel:+994 12 525 9001" title="">
+                  +994 12 525 9001
+                </a>
+                <a href="tel:+994 50 833 9010" title="">
+                  +994 50 833 9010
+                </a>
+                <a href="tel:+994 99 833 9010" title="">
+                  +994 99 833 9010
+                </a>
+              </div>
+            </li>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -2,56 +2,84 @@ import "./Header.scss";
 import phone from "./phone.svg";
 import logo from "./logo.png";
 
-
-
 export const Header = () => {
   return (
-    <header className="header">
+    <header className="header header-inside">
       <div className="topHeader">
-        <div className="headerTopLeft">
-          <select className="langOpt" id="lang">
-            <option value="AZ">AZ</option>
-            <option value="RU">RU</option>
-            <option value="ENG">ENG</option>
-          </select>
-          <div className="header-phone">
-            <img src={phone} alt="" />
-            <a href="tel:+994 12 525 9001" title="">
-              +994 12 525 9001
-            </a>
-            <a href="tel:+994 50 833 9010" title="">
-              +994 50 833 9010
-            </a>
-            <a href="tel:+994 99 833 9010" title="">
-              +994 99 833 9010
-            </a>
+        <div className="container">
+          <div className="header-top-inside">
+            <div className="headerTopLeft">
+              <select className="langOpt" id="lang">
+                <option value="AZ">AZ</option>
+                <option value="RU">RU</option>
+                <option value="ENG">ENG</option>
+              </select>
+              <div className="header-phone">
+                <img src={phone} alt="" />
+                <a href="tel:+994 12 525 9001" title="">
+                  +994 12 525 9001
+                </a>
+                <a href="tel:+994 50 833 9010" title="">
+                  +994 50 833 9010
+                </a>
+                <a href="tel:+994 99 833 9010" title="">
+                  +994 99 833 9010
+                </a>
+              </div>
+            </div>
+            <div className="header-top-right">
+              <a href="#signin" className="inline cboxElement" title="">
+                Daxil ol
+              </a>
+              <span className="random">/</span>
+              <a href="/az/signup.html" title="">
+                Qeydiyyat
+              </a>
+            </div>
           </div>
         </div>
-        <div className="header-top-right">
-          <a href="#signin" className="inline cboxElement" title="">
-            Daxil ol
-          </a>
-          <span className="random">/</span>
-          <a href="/az/signup.html" title="">
-            Qeydiyyat
-          </a>
-        </div>
       </div>
+
       <div className="bottomHeader">
-        <a href="#/">
-          <img src={logo} alt="" />
-        </a>
-        <div className="headerMenu desktopOnly">
-        <ul className="headerList">
-          <li><a href="/tariff.html" title="">Tariflər</a></li>
-          <li><a href="/store.html" title="">Mağazalar</a></li>
-          <li><a href="/faq.html" title="">Faq</a></li>
-          <li><a href="/contact.html" title="">Əlaqə</a></li>
-          <li><a href="/az/menu/maxfilik-32.html" title="">Məxfilik</a></li>
-        </ul>
+        <div className="container">
+          <div className="header-btm-inside">
+            <div className="header-logo">
+              <a href="#/">
+                <img src={logo} alt="" />
+              </a>
+            </div>
+            <div className="headerMenu desktopOnly">
+              <ul className="headerList">
+                <li>
+                  <a href="/tariff.html" title="">
+                    Tariflər
+                  </a>
+                </li>
+                <li>
+                  <a href="/store.html" title="">
+                    Mağazalar
+                  </a>
+                </li>
+                <li>
+                  <a href="/faq.html" title="">
+                    Faq
+                  </a>
+                </li>
+                <li>
+                  <a href="/contact.html" title="">
+                    Əlaqə
+                  </a>
+                </li>
+                <li>
+                  <a href="/az/menu/maxfilik-32.html" title="">
+                    Məxfilik
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-      
     </header>
   );
 };
