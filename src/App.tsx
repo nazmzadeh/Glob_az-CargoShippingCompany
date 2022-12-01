7e60adc4507e228fd64b31ed7692059ccbabb88e
Kerim <alekperovkerim@gmail.com>
2022-12-01 14:06:42 +0400

    добавил линк

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Header } from './pages/Header/Header';
-import { Footer } from './pages/Footer/Footer';
-import { Home } from './pages/HomePage/Home';
-import { Tariffs } from './pages/TariffsPage/Tariffs';
-import { Stores } from './pages/StoresPage/Stores';
-import { Faq } from './pages/FaqPage/Faq';
-import { Contact } from './pages/ContactPage/Contact';
-import { Confidentiality } from './pages/ConfidentialityPage/Confidentiality';
-
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Header } from "./pages/Header/Header";
+import { Footer } from "./pages/Footer/Footer";
+import { Home } from "./pages/HomePage/Home";
+import { Tariffs } from "./pages/TariffsPage/Tariffs";
+import { Stores } from "./pages/StoresPage/Stores";
+import { Faq } from "./pages/FaqPage/Faq";
+import { Contact } from "./pages/ContactPage/Contact";
+import { Confidentiality } from "./pages/ConfidentialityPage/Confidentiality";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <main>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        {/* <main>
         <Home />
         <Tariffs />
+        
         <Stores />
         <Faq />
         <Contact />
         <Confidentiality />
-      </main>
-      <Footer />
-    </div>
+      </main> */}
+        <Routes>
+          {/* <Route path="/" element={<Dashboard />}>
+            <Route path="messages" element={<DashboardMessages />} />
+            <Route path="tasks" element={<DashboardTasks />} />
+          </Route> */}
+          <Route path="tarifler" element={<Tariffs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 }
 
