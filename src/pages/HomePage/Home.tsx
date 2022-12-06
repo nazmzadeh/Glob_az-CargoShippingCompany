00bdcf34e3a6b832f8ee97e75702abee98852a5c
Naz Mzadeh <7QD1XHL@code.edu.az>
2022-12-06 12:04:04 +0400

    steps section unfinished

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -3,12 +3,18 @@ import { HeroSlider } from './components/Hero-Slider/HeroSlider';
 import { News } from './components/News/News';
 import { Shops } from './components/Shops/Shops';
 import { Step } from './components/Step/Step';
+import slideImg1 from "./components/Hero-Slider/images/sliderglobbi.png";
+import slideImg2 from "./components/Hero-Slider/images/tetbiq.png";
 import './Home.scss'
 
 export const Home=()=>{
+    let slideItems = [
+    { id: 1, title: "Globbi Sizi salamlayır!",subTitle:'Peşəkar komandamızla xidmətinizdəyik.',caption: "Globbinin boyu o qədər uzundur ki, Sizin problemlərinizi vaxtında görəcək və məmnuniyyətiniz üçün problemlərinizi tez bir zamanda həll edəcək.",imageSrc:slideImg1 },
+    { id: 2, title: "Artıq hər kəs üçün tətbiqimiz var!",subTitle:'Peşəkar komandamızla xidmətinizdəyik.',caption: 'İOS və ANDROİD istifadəçiləri üçün',imageSrc:slideImg2 },
+  ];
   return(
     <>
-    <HeroSlider/>
+    <HeroSlider />
     <Step/>
     <Calculator/>
     <News/>
--- src/pages/HomePage/components/Step/Step.tsx
+++ src/pages/HomePage/components/Step/Step.tsx
@@ -1,7 +1,42 @@
 import './Step.scss'
+import deliveryImg from './images/step.svg'
+import orderIcon from './images/nece-sifarish-edim-01-01-1.svg'
+import payIcon from './images/odeme-01.svg'
+import deliveryIcon from './images/catdirilma-01.svg'
 
-export const Step=()=>{
-  return(
-    <p></p>
+export const Step = () => {
+  return (
+    <section id='step'>
+      <h2>SİZ ALIRSINIZ,BİZ ÇATDIRIRIQ</h2>
+      <div className="flex-container">
+        <img className='main_img' src={deliveryImg} alt="delivery" />
+        <div className="steps">
+          <div className="how_to_order step">
+            <img src={orderIcon} alt="how to order" />
+            <div className="text">
+              <h5>Necə sifariş edim?</h5>
+              <p>Məhsulu seçin, Ödəniş edin, Məhsulu biz alırıq. </p>-
+              <a href='#'>Daha çox..</a>
+            </div>
+          </div>
+          <div className="pay step">
+            <img src={payIcon} alt="how to order" />
+            <div className="text">
+              <h5>Ödəmə</h5>
+              <p>Visa, Master bank kartları. Bank kartları olmayanlara yerində nağd ödəniş. </p>-
+              <a href='#'>Daha çox..</a>
+            </div>
+          </div>
+          <div className="delivery step">
+            <img src={deliveryIcon} alt="how to order" />
+            <div className="text">
+              <h5>Çatdırılma</h5>
+              <p>Bağlama Türkiyə anbarına qəbul edilir. Yüklər qablaşdırılır və  Azərbaycana göndərilir. Bakı ofisindən bağlamalar təhvil verilir. </p>-
+              <a href='#'>Daha çox..</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
