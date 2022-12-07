5e7aaf95e620a2b61f99dc9789c8984eed13b25c
Kerim-tk <alekperovkerim@gmail.com>
2022-12-07 10:00:21 +0400

    add Ъьъ

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import store from './app/store';
+import { store } from './app/store';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
